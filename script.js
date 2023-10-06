0ec10aced276f3cc32d6e8b40bbfff40fc0f60ec
Rbsilva2023 <robsonchristopher2022@gmail.com>
2023-10-06 14:48:41 -0300

    finalizado

--- script.js
+++ script.js
@@ -43,39 +43,45 @@ function renderElements (arr) {
 })
 }
 
-function createTaskItem (object) {
+function createTaskItem(object, index) {
+  const liList = document.createElement("li");
+  const divList = document.createElement("div");
+  const spanList = document.createElement("span");
+  const pList = document.createElement("p");
+  const buttonList = document.createElement("button");
 
-  const liList = document.createElement("li")
-  const divList = document.createElement("div")
-  const spanList = document.createElement("span")
-  const pList = document.createElement("p")
-  const buttonList = document.createElement("button")
+  liList.classList.add("task__item");
+  divList.classList.add("task-info__container");
+  spanList.classList.add("task-type");
+  buttonList.classList.add("task__button--remove-task");
 
-  
-
-  liList.classList.add("task__item")
-  divList.classList.add("task-info__container")
-  spanList.classList.add("task-type")
-  buttonList.classList.add("task__button--remove-task")
-
-  if(object.type === "Urgente"){
-    spanList.classList.add("span-urgent")
-  } else if (object.type === "Importante"){
-    spanList.classList.add("span-important")
-  } else if (object.type === "Normal"){
-    spanList.classList.add("span-normal")
+  if (object.type === "Urgente") {
+    spanList.classList.add("span-urgent");
+  } else if (object.type === "Importante") {
+    spanList.classList.add("span-important");
+  } else if (object.type === "Normal") {
+    spanList.classList.add("span-normal");
   }
 
-  pList.innerText = object.title
+  pList.innerText = object.title;
 
-  divList.append(spanList,pList)
-  liList.append(divList,buttonList)
+  
+  buttonList.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+  buttonList.classList.add("task__button--remove-task");
+
+  
+  buttonList.addEventListener('click', function () {
+    const index = tasks.indexOf(object);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      renderElements(tasks);
+    }
+  });
+
+  divList.append(spanList, pList);
+  liList.append(divList, buttonList);
 
   return liList;
-
-  
-  
 }
-
 
 renderElements(tasks)
